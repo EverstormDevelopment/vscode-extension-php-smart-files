244ddda25af8cd35dc45f3201d021a6053a9c661
Nico Th. Stolz <nico@everstorm.de>
2025-04-30 10:57:46 +0200

    Update InputBoxFactory.ts

--- src/service/input/build/InputBoxFactory.ts
+++ src/service/input/build/InputBoxFactory.ts
@@ -1,6 +1,11 @@
+import * as vscode from "vscode";
 import { InputBoxTypeEnum } from "../enum/InputBoxTypeEnum";
 import { InputBoxFactoryInterface } from "../interface/InputBoxFactoryInterface";
 import { InputBoxInterface } from "../interface/InputInterface";
+import { InputPhpFileNameProcessor } from "../processor/InputPhpFileNameProcessor";
+import { InputDefinitionNameValidator } from "../validator/InputDefinitionNameValidator";
+import { InputFileNameValidator } from "../validator/InputFileNameValidator";
+import { InputBoxBuilder } from "./InputBoxBuilder";
 
 /**
  * Factory for creating input boxes based on type
@@ -8,14 +13,14 @@ import { InputBoxInterface } from "../interface/InputInterface";
  */
 export class InputBoxFactory implements InputBoxFactoryInterface {
     /**
-     * Map of factory functions for different input box types
+     * Map of factory methods for different input box types
      */
-    private readonly factories: Record<InputBoxTypeEnum, () => InputBoxInterface> = {
-        [InputBoxTypeEnum.File]: () => new InputBoxFileFactory().create(),
-        [InputBoxTypeEnum.Class]: () => new InputBoxClassFactory().create(),
-        [InputBoxTypeEnum.Interface]: () => new InputBoxClassFactory().create(),
-        [InputBoxTypeEnum.Enum]: () => new InputBoxClassFactory().create(),
-        [InputBoxTypeEnum.Trait]: () => new InputBoxClassFactory().create()
+    private readonly creators: Record<InputBoxTypeEnum, () => InputBoxInterface> = {
+        [InputBoxTypeEnum.File]: () => this.createFileInputBox(),
+        [InputBoxTypeEnum.Class]: () => this.createClassInputBox(),
+        [InputBoxTypeEnum.Interface]: () => this.createInterfaceInputBox(),
+        [InputBoxTypeEnum.Enum]: () => this.createEnumInputBox(),
+        [InputBoxTypeEnum.Trait]: () => this.createTraitInputBox()
     };
 
     /**
@@ -25,10 +30,80 @@ export class InputBoxFactory implements InputBoxFactoryInterface {
      * @throws Error if the specified type is not supported
      */
     public create(type: InputBoxTypeEnum): InputBoxInterface {
-        const factory = this.factories[type];
-        if (!factory) {
+        const creator = this.creators[type];
+        if (!creator) {
             throw new Error(`Unknown input box type: ${type}`);
         }
-        return factory();
+        return creator();
+    }
+
+    /**
+     * Creates an input box configured for PHP file creation
+     * @returns An input box interface implementation for PHP files
+     */
+    private createFileInputBox(): InputBoxInterface {
+        return new InputBoxBuilder()
+            .setTitle(vscode.l10n.t("Create PHP File"))
+            .setPlaceholder(vscode.l10n.t("Enter filename (without .php)"))
+            .setPrompt(vscode.l10n.t("Enter a name for the new PHP file"))
+            .setInputValidator(new InputFileNameValidator())
+            .setInputProcessor(new InputPhpFileNameProcessor())
+            .build();
+    }
+
+    /**
+     * Creates an input box configured for PHP class creation
+     * @returns An input box interface implementation for PHP classes
+     */
+    private createClassInputBox(): InputBoxInterface {
+        return new InputBoxBuilder()
+            .setTitle(vscode.l10n.t("Create PHP Class"))
+            .setPlaceholder(vscode.l10n.t("Enter class name"))
+            .setPrompt(vscode.l10n.t("Enter a name for the new PHP class"))
+            .setInputValidator(new InputDefinitionNameValidator())
+            .setInputProcessor(new InputPhpFileNameProcessor())
+            .build();
+    }
+
+    /**
+     * Creates an input box configured for PHP interface creation
+     * @returns An input box interface implementation for PHP interfaces
+     */
+    private createInterfaceInputBox(): InputBoxInterface {
+        return new InputBoxBuilder()
+            .setTitle(vscode.l10n.t("Create PHP Interface"))
+            .setPlaceholder(vscode.l10n.t("Enter interface name"))
+            .setPrompt(vscode.l10n.t("Enter a name for the new PHP interface"))
+            .setInputValidator(new InputDefinitionNameValidator())
+            .setInputProcessor(new InputPhpFileNameProcessor())
+            .build();
+    }
+
+    /**
+     * Creates an input box configured for PHP enum creation
+     * @returns An input box interface implementation for PHP enums
+     */
+    private createEnumInputBox(): InputBoxInterface {
+        return new InputBoxBuilder()
+            .setTitle(vscode.l10n.t("Create PHP Enum"))
+            .setPlaceholder(vscode.l10n.t("Enter enum name"))
+            .setPrompt(vscode.l10n.t("Enter a name for the new PHP enum"))
+            .setInputValidator(new InputDefinitionNameValidator())
+            .setInputProcessor(new InputPhpFileNameProcessor())
+            .build();
+    }
+
+    /**
+     * Creates an input box configured for PHP trait creation
+     * @returns An input box interface implementation for PHP traits
+     */
+    private createTraitInputBox(): InputBoxInterface {
+        return new InputBoxBuilder()
+            .setTitle(vscode.l10n.t("Create PHP Trait"))
+            .setPlaceholder(vscode.l10n.t("Enter trait name"))
+            .setPrompt(vscode.l10n.t("Enter a name for the new PHP trait"))
+            .setInputValidator(new InputDefinitionNameValidator())
+            .setInputProcessor(new InputPhpFileNameProcessor())
+            .build();
     }
 }

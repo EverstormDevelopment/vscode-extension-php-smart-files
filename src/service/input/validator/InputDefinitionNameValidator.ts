7cd1a312045c1508ddc99fd0ea3da1af2c15d3b4
Nico Th. Stolz <nico@everstorm.de>
2025-04-30 11:44:29 +0200

    Update InputDefinitionNameValidator.ts

--- src/service/input/validator/InputDefinitionNameValidator.ts
+++ src/service/input/validator/InputDefinitionNameValidator.ts
@@ -6,6 +6,85 @@ import { InputValidatorInterface } from "../interface/InputValidatorInterface";
  */
 export class InputDefinitionNameValidator implements InputValidatorInterface {
     /**
+     * List of PHP reserved keywords
+     */
+    private readonly reservedKeywords = [
+        "abstract",
+        "and",
+        "array",
+        "as",
+        "break",
+        "callable",
+        "case",
+        "catch",
+        "class",
+        "clone",
+        "const",
+        "continue",
+        "declare",
+        "default",
+        "die",
+        "do",
+        "echo",
+        "else",
+        "elseif",
+        "empty",
+        "enddeclare",
+        "endfor",
+        "endforeach",
+        "endif",
+        "endswitch",
+        "endwhile",
+        "eval",
+        "exit",
+        "extends",
+        "final",
+        "finally",
+        "fn",
+        "for",
+        "foreach",
+        "function",
+        "global",
+        "goto",
+        "if",
+        "implements",
+        "include",
+        "include_once",
+        "instanceof",
+        "insteadof",
+        "interface",
+        "isset",
+        "list",
+        "match",
+        "namespace",
+        "new",
+        "or",
+        "print",
+        "private",
+        "protected",
+        "public",
+        "require",
+        "require_once",
+        "return",
+        "static",
+        "switch",
+        "throw",
+        "trait",
+        "try",
+        "unset",
+        "use",
+        "var",
+        "while",
+        "xor",
+        "yield",
+        "enum",
+    ];
+
+    public constructor(
+        private allowLowercaseStart: boolean = false,
+    ) {}
+
+    /**
      * Validates a PHP definition name
      * @param input The input to validate
      * @returns Error message or empty string if valid
@@ -20,84 +99,19 @@ export class InputDefinitionNameValidator implements InputValidatorInterface {
             return vscode.l10n.t("Definition name must start with a letter or underscore");
         }
 
+        if (!this.allowLowercaseStart) {
+            const uppercaseStartRegex = /^[\p{Lu}_]/u;
+            if (!uppercaseStartRegex.test(input)) {
+                return vscode.l10n.t("Definition name must start with an uppercase letter or underscore");
+            }
+        }
+
         const validNameRegex = /^[\p{L}_][\p{L}\p{N}_]*$/u;
         if (!validNameRegex.test(input)) {
             return vscode.l10n.t("Definition name can only contain letters, numbers, and underscores");
         }
 
-        const reservedKeywords = [
-            "abstract",
-            "and",
-            "array",
-            "as",
-            "break",
-            "callable",
-            "case",
-            "catch",
-            "class",
-            "clone",
-            "const",
-            "continue",
-            "declare",
-            "default",
-            "die",
-            "do",
-            "echo",
-            "else",
-            "elseif",
-            "empty",
-            "enddeclare",
-            "endfor",
-            "endforeach",
-            "endif",
-            "endswitch",
-            "endwhile",
-            "eval",
-            "exit",
-            "extends",
-            "final",
-            "finally",
-            "fn",
-            "for",
-            "foreach",
-            "function",
-            "global",
-            "goto",
-            "if",
-            "implements",
-            "include",
-            "include_once",
-            "instanceof",
-            "insteadof",
-            "interface",
-            "isset",
-            "list",
-            "match",
-            "namespace",
-            "new",
-            "or",
-            "print",
-            "private",
-            "protected",
-            "public",
-            "require",
-            "require_once",
-            "return",
-            "static",
-            "switch",
-            "throw",
-            "trait",
-            "try",
-            "unset",
-            "use",
-            "var",
-            "while",
-            "xor",
-            "yield",
-            "enum",
-        ];
-
-        if (reservedKeywords.includes(input.toLowerCase())) {
+        if (this.reservedKeywords.includes(input.toLowerCase())) {
             return vscode.l10n.t("Cannot use PHP reserved keyword as definition name");
         }
 

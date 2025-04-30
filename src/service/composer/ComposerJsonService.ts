b24dab5d7780032a8cbdc9baef7d318fc54f2b22
Nico Th. Stolz <nico@everstorm.de>
2025-04-30 19:47:11 +0200

    Update ComposerJsonService.ts

--- src/service/composer/ComposerJsonService.ts
+++ src/service/composer/ComposerJsonService.ts
@@ -1,4 +1,3 @@
-import * as path from "path";
 import * as vscode from "vscode";
 import { ComposerJsonFinder } from "./ComposerJsonFinder";
 import { ComposerJsonParser } from "./ComposerJsonParser";
@@ -7,22 +6,32 @@ import { ComposerJsonParser } from "./ComposerJsonParser";
  * Service for resolving and parsing composer.json files in PHP projects
  */
 export class ComposerJsonService {
+    /**
+     * Creates a new ComposerJsonService
+     * @param composerJsonFinder The finder for composer.json files
+     * @param composerJsonParser The parser for composer.json files
+     */
     constructor(
         private readonly composerJsonFinder: ComposerJsonFinder,
         private readonly composerJsonParser: ComposerJsonParser
     ) {}
 
     /**
-     * Finds and parses a composer.json file starting from the target URI
-     * @param targetUri The URI to start searching from (file or directory)
-     * @returns Parsed composer.json content as an object
-     * @throws Error if composer.json cannot be found or parsed
+     * Finds a composer.json file starting from the target URI
+     * @param targetUri The URI to start searching from
+     * @returns URI of the found composer.json file or undefined if not found
      */
-    public async findAndParse(targetUri: vscode.Uri): Promise<any> {
-        const composerJsonUri = await this.composerJsonFinder.find(targetUri);
-        if (!composerJsonUri) {
-            throw new Error("Could not find composer.json file");
-        }
+    public async find(targetUri: vscode.Uri): Promise<vscode.Uri | undefined> {
+        return this.composerJsonFinder.find(targetUri);
+    }
+
+    /**
+     * Parses a composer.json file
+     * @param composerJsonUri URI of the composer.json file to parse
+     * @returns Parsed composer.json content
+     * @throws Error if the file cannot be read or parsed
+     */
+    public async parse(composerJsonUri: vscode.Uri): Promise<any> {
         return this.composerJsonParser.parse(composerJsonUri);
     }
 }

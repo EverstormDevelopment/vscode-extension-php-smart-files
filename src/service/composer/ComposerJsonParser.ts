61711a01d58a8d7ab27a5cfe9392f0860dfab663
Nico Th. Stolz <nico@everstorm.de>
2025-05-04 01:33:35 +0200

    Update ComposerJsonParser.ts

--- src/service/composer/ComposerJsonParser.ts
+++ src/service/composer/ComposerJsonParser.ts
@@ -1,25 +1,25 @@
 import * as vscode from "vscode";
 import { isFile } from "../../utils/filesystem/isFile";
+import { ComposerJsonType } from "./type/ComposerJsonType";
 
 /**
  * Service to parse composer.json files for PHP projects.
  */
 export class ComposerJsonParser {
     /**
-     * Parses a composer.json file into a JavaScript object and balidates that
-     * the file exists before attempting to parse it.
+     * Parses a composer.json file into a JavaScript object.
      * @param composerJsonUri URI of the composer.json file to parse
      * @returns Promise resolving to the parsed composer.json content as an object
      * @throws Error if the file cannot be read or parsed
      */
-    public async parse(composerJsonUri: vscode.Uri): Promise<any> {
+    public async parse(composerJsonUri: vscode.Uri): Promise<ComposerJsonType> {
         try {
             if (!(await isFile(composerJsonUri))) {
                 throw new Error(`Path is not a file: ${composerJsonUri.fsPath}`);
             }
 
             const content = await vscode.workspace.fs.readFile(composerJsonUri);
-            return JSON.parse(Buffer.from(content).toString("utf8"));
+            return JSON.parse(Buffer.from(content).toString("utf8")) as ComposerJsonType;
         } catch (error: unknown) {
             this.showErrorMessage(vscode.l10n.t("Error reading composer.json"));
             throw error;

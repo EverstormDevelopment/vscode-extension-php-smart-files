a27fc60fd67ddcaeff7c4c673a72a98e7a2310c4
Nico Th. Stolz <nico@everstorm.de>
2025-05-04 00:44:32 +0200

    Improve imports and formatting

--- src/service/composer/type/AutoloadConfigType.ts
+++ src/service/composer/type/AutoloadConfigType.ts
@@ -1,10 +1,10 @@
 /**
  * Represents a mapping from namespace prefixes to directory paths
  * for PSR-0 or PSR-4 autoloading in composer.json.
- * 
+ *
  * The key is the namespace prefix (e.g., "App\\") and the value is an array of directories
  * where classes in that namespace can be found.
- * 
+ *
  * @example
  * {
  *   "App\\": ["src/"],
--- src/service/composer/type/AutoloadConfigsType.ts
+++ src/service/composer/type/AutoloadConfigsType.ts
@@ -3,7 +3,7 @@ import { AutoloadConfigType } from "./AutoloadConfigType";
 /**
  * Contains the PSR-0 and PSR-4 autoloading configurations from composer.json.
  * This type organizes autoload configurations by their PSR standard type.
- * 
+ *
  * @example
  * {
  *   psr4: { "App\\": ["src/"] },
@@ -14,10 +14,10 @@ export type AutoloadConfigsType = {
     /**
      * PSR-4 autoloading configuration where namespace prefixes map directly to directories
      */
-    psr4: AutoloadConfigType
+    psr4: AutoloadConfigType;
 
     /**
      * PSR-0 autoloading configuration with underscores in class names mapping to directory separators
      */
-    psr0: AutoloadConfigType
-}
+    psr0: AutoloadConfigType;
+};

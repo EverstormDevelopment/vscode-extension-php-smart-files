e2561a93ff677718768c4f6b4228c867eb3bac6c
Nico Th. Stolz <nico@everstorm.de>
2025-05-08 03:25:42 +0200

    style: Format code with Prettier

--- src/extension/registry/FileObserverRegistry.ts
+++ src/extension/registry/FileObserverRegistry.ts
@@ -2,7 +2,6 @@ import { ConstructorType } from "../../container/type/ConstructorType";
 import { FileMovedObserver } from "../observer/FileMovedObserver";
 import { FileRenamedObserver } from "../observer/FileRenamedObserver";
 
-
 /**
  * Registry for all file observers in the application that should
  * be started when the extension is activated.

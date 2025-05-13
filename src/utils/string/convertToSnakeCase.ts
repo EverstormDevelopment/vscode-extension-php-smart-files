36d9acd956bbc4ccf9d2918e5e8cfae8441ad470
Nico Th. Stolz <nico@everstorm.de>
2025-05-13 12:09:59 +0200

    doc: Removed a empty line

--- src/utils/string/convertToSnakeCase.ts
+++ src/utils/string/convertToSnakeCase.ts
@@ -1,17 +1,15 @@
 /**
  * Converts a PascalCase or camelCase string to snake_case
- * 
  * @param input - The PascalCase or camelCase string to convert
  * @returns The converted string in snake_case format
  */
 export function convertToSnakeCase(input: string): string {
-    if (!input){
-         return '';
+    if (!input) {
+        return "";
     }
-    
+
     const result = input
-        .replace(/(\p{Ll})(\p{Lu})/gu, '$1_$2')
-        .replace(/(\p{Lu}+)(\p{Lu}\p{Ll})/gu, '$1_$2');
-    
+        .replace(/(\p{Ll})(\p{Lu})/gu, "$1_$2")
+        .replace(/(\p{Lu}+)(\p{Lu}\p{Ll})/gu, "$1_$2");
     return result.toLowerCase();
 }

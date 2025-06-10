adeb59c51bdfb16c06f2fdab56032c210b893278
Nico Th. Stolz <nico@everstorm.de>
2025-06-10 15:37:23 +0200

    feat: Add a simple php-parser

---
+++ src/service/parser/PhpParser.ts
@@ -0,0 +1,108 @@
+import type {
+    Program as AST,
+    Node as PhpNode,
+    Namespace as PhpNamespace,
+    Declaration as PhpDeclaration,
+    ConstantStatement as PhpConstantStatement,
+} from "php-parser";
+import { Engine } from "php-parser";
+import { DeclarationType } from "./type/DeclarationType";
+
+export class PhpParser {
+    private ast: AST;
+
+    constructor(phpCode: string, fileName: string = "") {
+        const parser = new Engine({
+            parser: {
+                php7: true,
+                suppressErrors: true,
+            },
+            ast: {
+                withPositions: true,
+            },
+        });
+
+        this.ast = parser.parseCode(phpCode, fileName);
+    }
+
+    public getAST(): AST {
+        return this.ast;
+    }
+
+    public getNamespace(): string | undefined {
+        if (!this.ast || !this.ast.children) {
+            return undefined;
+        }
+
+        const namespaceNode = this.getNamespaceNode();
+        if (!namespaceNode) {
+            return undefined;
+        }
+
+        return namespaceNode.name || undefined;
+    }
+
+    public getTopLevelDeclarations(): any[] {
+        const namespaceNode = this.getNamespaceNode();
+        if (!namespaceNode) {
+            return [];
+        }
+
+        const declarations = [];
+        for (const node of namespaceNode.children) {
+            switch (node.kind) {
+                case "class":
+                case "interface":
+                case "enum":
+                case "trait":
+                case "function":
+                    const declaration = this.getDeclarationFromNode(node);
+                    declarations.push(declaration);
+                    break;
+                case "constantstatement":
+                    const constantDeclarations = this.getConstantDeclarationsFromNode(node);
+                    declarations.push(...constantDeclarations);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return declarations;
+    }
+
+    private getNamespaceNode(): PhpNamespace | undefined {
+        if (!this.ast || !this.ast.children) {
+            return undefined;
+        }
+
+        for (const node of this.ast.children) {
+            if (node.kind === "namespace") {
+                return node as PhpNamespace;
+            }
+        }
+
+        return undefined;
+    }
+
+    private getConstantDeclarationsFromNode(node: PhpNode): DeclarationType[] {
+        const constantNode = node as PhpConstantStatement;
+        const declarations: DeclarationType[] = [];
+
+        for (const child of constantNode.constants) {
+            if (child.kind === "constant") {
+                const declaration = this.getDeclarationFromNode(child);
+                declarations.push(declaration);
+            }
+        }
+        return declarations;
+    }
+
+    private getDeclarationFromNode(node: PhpNode): DeclarationType {
+        const declarationNode = node as PhpDeclaration;
+        const identifier = typeof declarationNode.name === "string" ? declarationNode.name : declarationNode.name.name;
+        return {
+            identifier: identifier,
+            type: node.kind,
+        };
+    }
+}
---
+++ src/service/parser/type/DeclarationType.ts
@@ -0,0 +1,4 @@
+export type DeclarationType = {
+    identifier: string;
+    type: string;
+}
